df6c3416e3b99223553e2e8752eea93290a19ec1
thiendang <thiendh310@gmail.com>
2025-04-29 11:51:14 +0700

    feat(middleware): middleware that navigates user reqs and builds management UI

---
+++ src/middleware.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+const privatePaths = ['/manage']
+const unAuthPaths = ['/login']
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl
+  // pathname: /manage/dashboard
+  const isAuth = Boolean(request.cookies.get('accessToken')?.value)
+  // Chưa đăng nhập thì không cho vào private paths
+  if (privatePaths.some((path) => pathname.startsWith(path)) && !isAuth) {
+    return NextResponse.redirect(new URL('/login', request.url))
+  }
+  // Đăng nhập rồi thì sẽ không cho vào login nữa
+  if (unAuthPaths.some((path) => pathname.startsWith(path)) && isAuth) {
+    return NextResponse.redirect(new URL('/', request.url))
+  }
+  return NextResponse.next()
+}
+
+// See "Matching Paths" below to learn more
+// middleware này sẽ áp dụng cho những path đươc khai báo bên dưới
+export const config = {
+  matcher: ['/manage/:path*', '/login']
+}
